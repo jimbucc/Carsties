bd742095015fce50526a1f66f8cee8b1405b3f95
jimbucc <James.Bucciferro@PERKINELMER.COM>
2023-11-11 08:34:41 -0500

    End of Section 6

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AuctionService.Data;
 using AuctionService.DTOs;
 using AuctionService.Entities;
