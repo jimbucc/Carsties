b56d4371cde72216a295af4e2f6eb57245bd2d58
jimbucc <James.Bucciferro@PERKINELMER.COM>
2023-12-02 08:36:17 -0500

    End of Section 11

---
+++ src/BiddingService/Models/Auction.cs
@@ -0,0 +1,12 @@
+﻿using MassTransit.Introspection;
+using MongoDB.Entities;
+
+namespace BiddingService;
+
+public class Auction : Entity
+{
+    public DateTime AuctionEnd { get; set; }
+    public string Seller { get; set; }
+    public int ReservedPrice { get; set; }
+    public bool Finished { get; set; }
+}
